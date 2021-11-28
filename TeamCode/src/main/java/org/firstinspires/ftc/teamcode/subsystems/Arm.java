9798462f8987128f1c083188f5e74fdc8a182078
CassieColby <cassiecolby@gmail.com>
2021-11-27 17:23:15 -0800

    Arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,30 +1,45 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Robot;
-
+//test
 public class Arm implements SubSystems {
     public DcMotor motor;
-    public DcMotor motor2;
+    public DcMotorEx motor2;
     public DcMotorEx armMotor;
+    public CRServo servo;
     Robot robot;
-    double power ;
+    double power;
     int current;
+    int motorPosition;
+    int flipPosition;
 
     public Arm(Robot robot) {
         this.robot = robot;
         motor = robot.hardwareMap.get(DcMotor.class, "arm_motor");
-        motor2 = robot.hardwareMap.get(DcMotor.class, "arm_motor2");
+        motor2 = robot.hardwareMap.get(DcMotorEx.class, "arm_motor2");
         armMotor = robot.hardwareMap.get(DcMotorEx.class, "intake_motor");
+        servo = robot.hardwareMap.get(CRServo.class, "intake_servo" );
+        motorPosition = 1000;
+        flipPosition = 1000;
     }
 
     @Override
     public void update() {
+        servo.setPower(1);
         motor.setPower(power);
         motor2.setPower(-1 * power);
-        armMotor.setTargetPosition(current);
+        if(motor2.getTargetPosition() >= motorPosition)
+        {
+            armMotor.setTargetPosition(motorPosition);
+        }
+        else
+        {
+            armMotor.setTargetPosition(0);
+        }
     }
 
     public void setPower(double setPower) {
