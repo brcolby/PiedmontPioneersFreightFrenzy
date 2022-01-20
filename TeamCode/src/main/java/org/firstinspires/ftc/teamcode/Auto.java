05840c538186ea60a9680ed1559d4264dcd21040
CassieColby <cassiecolby@gmail.com>
2022-01-19 19:17:32 -0800

    auto values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -4,28 +4,39 @@ import android.os.Build;
 
 import androidx.annotation.RequiresApi;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-
-@TeleOp(name = "Auto", group = "Linear Opmode")
-    public class Auto extends LinearOpMode {
+public class Auto {
+    @TeleOp(name = "Auto", group = "Linear Opmode")
+    public class FrightFrenzyTeleOp extends LinearOpMode {
         @RequiresApi(api = Build.VERSION_CODES.N)
         public void runOpMode() {
             Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
 
   //          robot.arm.flipBucket.setPosition(0.0);
             waitForStart();
-            robot.drive.setLeftPower(0.5, false);
-            robot.drive.setRightPower(0.5, false );
-            sleep(2000);
-            robot.drive.setLeftPower(0.0, false);
-            robot.drive.setRightPower(0.0, false );
-            robot.arm.armSpeed(-0.3);
-            sleep(500);
-            robot.arm.intake(-0.3);
-            sleep(500);
-            robot.arm.intake(0);
+            while(opModeIsActive()) {
+                robot.drive.setLeftPower(0.5, false);
+                robot.update();
+                robot.drive.setRightPower(0.5, false);
+                robot.update();
+                sleep(2000);
+                robot.drive.setLeftPower(0.0, false);
+                robot.update();
+                robot.drive.setRightPower(0.0, false);
+                robot.update();
+                robot.arm.armSpeed(-0.3);
+                robot.update();
+                sleep(500);
+                robot.update();
+                robot.arm.intake(-0.3);
+                robot.update();
+                sleep(500);
+                robot.update();
+                robot.arm.intake(0);
+                robot.update();
+            }
         }
     }
+}
