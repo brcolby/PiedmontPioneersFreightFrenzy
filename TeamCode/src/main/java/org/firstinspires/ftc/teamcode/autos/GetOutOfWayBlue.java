7a33f1d337211985d61f9fcaee13298980cba83d
Edward-Ba <ebalasingham@gmail.com>
2022-02-07 16:27:53 -0800

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/GetOutOfWayBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/GetOutOfWayBlue.java
@@ -23,7 +23,7 @@ public class GetOutOfWayBlue extends LinearOpMode {
 
         TrajectorySequence trajectory = drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(-90)))
                 .forward(2)
-                .turn(Math.toRadians(-10.5))
+                .turn(Math.toRadians(10.5))
                 .forward(4)
                 .addDisplacementMarker(() -> state = State.STOPPED)
                 .build();
