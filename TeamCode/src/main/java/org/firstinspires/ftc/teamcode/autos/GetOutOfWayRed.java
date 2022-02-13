d52a45660a40d347738dbd14e59edfd491157bba
Edward-Ba <ebalasingham@gmail.com>
2022-02-12 17:09:03 -0800

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/GetOutOfWayRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/GetOutOfWayRed.java
@@ -21,9 +21,9 @@ public class GetOutOfWayRed extends LinearOpMode {
         SampleTankDrive drive = new SampleTankDrive(hardwareMap);
 
         TrajectorySequence trajectory = drive.trajectorySequenceBuilder(new Pose2d(-35, 60, Math.toRadians(-90)))
-                .forward(2)
+                .forward(3)
                 .turn(Math.toRadians(10.5))
-                .forward(4)
+                .forward(6)
                 .addDisplacementMarker(() -> state = GetOutOfWayRed.State.STOPPED)
                 .build();
 
