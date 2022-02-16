e784e6c12c920c233a16388674cafe82cce5ba63
27Edward-Ba <ebalasingham@gmail.com>
2022-02-15 19:36:22 -0800

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DucksBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DucksBlue.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.autos;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -20,20 +21,21 @@ public class DucksBlue extends LinearOpMode {
 
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
-        waitForStart();
         SampleTankDrive drive = new SampleTankDrive(hardwareMap);
         TrajectorySequence trajectory = drive.trajectorySequenceBuilder(new Pose2d())
                 .forward(-2)
                 .turn(Math.toRadians(-5.25))
                 .forward(2.71)
                 .UNSTABLE_addTemporalMarkerOffset(0.1, ()-> state = State.CAROUSELON)
-                .UNSTABLE_addTemporalMarkerOffset(2.9, ()-> state = State.CAROUSELOFF)
                 .waitSeconds(3)
+                .UNSTABLE_addTemporalMarkerOffset(2.9, ()-> state = State.CAROUSELOFF)
+
                 .forward(-4)
                 .turn(Math.toRadians(-5.25))
                 .forward(2)
-                .addDisplacementMarker(() -> state = State.STOPPED)
                 .build();
+
+        waitForStart();
 
         drive.followTrajectorySequence(trajectory);
 
@@ -41,20 +43,16 @@ public class DucksBlue extends LinearOpMode {
             robot.update();
             switch (state) {
                 case STARTED:
-                    telemetry.addData("State ", state.name());
                     break;
                 case CAROUSELON:
-                    robot.carousel.setPowerAuto(0.5);
-                    telemetry.addData("State ", state.name());
+                    robot.carousel.setPower(true, true);
                     break;
                 case CAROUSELOFF:
-                    robot.carousel.setPowerAuto(0);
-                    telemetry.addData("State ", state.name());
+                    robot.carousel.setPower(false, true);
                     break;
                 case STOPPED:
-                    robot.carousel.setPowerAuto(0);
-                    telemetry.addData("State ", state.name());
-                }
+                    robot.carousel.setPower(false, false);
+            }
         }
     }
 }
