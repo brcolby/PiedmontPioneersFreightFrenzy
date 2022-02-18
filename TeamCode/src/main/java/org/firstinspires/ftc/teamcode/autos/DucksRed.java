71e99dc60bd82d495a9015c5e4d42e3f6fae2da2
CassieColby <cassiecolby@gmail.com>
2022-02-17 18:37:01 -0800

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DucksRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/DucksRed.java
@@ -10,13 +10,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 @TeleOp(name = "Ducks Red", group = "Linear Opmode")
 public class DucksRed extends LinearOpMode {
-    public enum State{
-        STARTED,
-        CAROUSELON,
-        CAROUSELOFF,
-        STOPPED
-    }
-    public State state = State.STARTED;
+
 
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap, gamepad1, gamepad2);
@@ -27,7 +21,6 @@ public class DucksRed extends LinearOpMode {
                 .turn(Math.toRadians(5.25))
                 .forward(2.71)
                 .waitSeconds(3)
-
                 .forward(-4)
                 .turn(Math.toRadians(5.25))
                 .forward(2)
@@ -38,28 +31,6 @@ public class DucksRed extends LinearOpMode {
         robot.update();
         drive.followTrajectorySequence(trajectory);
 
-        while (opModeIsActive()) {
-            robot.update();
-            switch (state) {
-                case STARTED:
-                    telemetry.addData("State ", state.name());
-                    break;
-                case CAROUSELON:
-                    robot.carousel.setPowerAuto(0.5);
-                    robot.update();
-                    telemetry.addData("State ", state.name());
-                    break;
-                case CAROUSELOFF:
-                    robot.carousel.setPowerAuto(0);
-                    robot.update();
-                    telemetry.addData("State ", state.name());
-                    break;
-                case STOPPED:
-                    robot.carousel.setPowerAuto(0);
-                    telemetry.addData("State ", state.name());
-                    robot.update();
-                    break;
-            }
-        }
+
     }
 }
